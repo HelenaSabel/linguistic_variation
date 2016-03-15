07a8fb419234040facf089ffbd5cf30951615c32
HelenaSabel <helena.b.sabel@gmail.com>
2016-03-15 19:32:16 -0400

    improved the selection of criteria and some other visual effects

--- HTML/javascript/checkboxes.js
+++ HTML/javascript/checkboxes.js
@@ -18,27 +18,6 @@ function changeCriteria() {
         
         if (this.checked == true) { for (var i = 0; i < dels.length; i++) {
                 dels[i].classList.remove('hide');
-                
-                if (dels[i].classList.contains('underdot')) {
-                    var letters = dels[i].textContent.split('');
-                    var output = '';
-                    for (var j = 0; j < letters.length; j++) {
-                        output += (letters[j] + '&#803;');
-                    }
-                    dels[i].innerHTML = output;
-                }
-                if (dels[i].classList.contains('multiple-overstrike')) {
-                    var letters = dels[i].textContent.split('');
-                    var output2 = '';
-                    for (var y = 0; y < letters.length; j++) {
-                        output += (letters[y] + '&#824;');
-                    }
-                    dels[i].innerHTML = output2;
-                }
-                
-                if (dels[i].classList.contains('scrape')) {
-                    dels[i].style.opacity = '0.9';
-                }
             }
         } else { for (var i = 0; i < dels.length; i++) {
                 dels[i].classList.add('hide');
